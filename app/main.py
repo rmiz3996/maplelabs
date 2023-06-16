7eddd589d13a726fb40a0ad3b5022331124eb241
rmiz3996 <rmiz3996@gmail.com>
2023-06-16 14:50:50 +0530

    third commit

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python! This is version2"
-
+    return "Hello from Python! This is version3"
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
