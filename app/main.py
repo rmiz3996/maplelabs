f6cce24ee76e9494c4ac62635f09110e9e898215
rmiz3996 <rmiz3996@gmail.com>
2023-06-16 15:37:31 +0530

    version five

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python! This is version4"
+    return "Hello from Python! This is version5"
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
